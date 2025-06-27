4243339fefe709498abec162b8accddbf48c962d
김영서 <youngseo.kim@saltlux.com>
2025-06-27 15:06:53 +0900

    Initial commit

---
+++ src/main/java/com/saltlux/kys/ElasticSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.saltlux.kys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saltlux/kys/ElasticSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saltlux.kys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
